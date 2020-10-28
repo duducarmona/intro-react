4d26e4948fceeb46f93db5a1716a82adc6673e74
Edu Carmona <dudu.carmona@gmail.com>
2020-10-28 13:33:32 +0100

    add Emotions

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
+import NavBar from "./NavBar";
 
 const App = () => {
+  const themeHook = useState('peru');
   return (
-    <div>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
-      <Router>
-        <SearchParams path="/" />
-        <Details path="/details/:id" />
-      </Router>
-    </div>
+    <React.StrictMode>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <NavBar />
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
+    </React.StrictMode>
   );
 };
 
